b2ec3e36780f14964f54f3c25a9d482e5e52f045
Anthonius Munthi <me@itstoni.com>
2023-11-13 01:34:34 +0800

    chore: removed storybook-static files

--- apps/storybook/storybook-static/assets/WithTooltip-4HIR6TLV-17a888a7.js
+++
@@ -1,2 +0,0 @@
-import{W as l,W as s,a as f}from"./DocsRenderer-NNNQARDV-5ee48ba5.js";import"./iframe-a370f03a.js";import"../sb-preview/runtime.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./_getPrototype-e1e43787.js";import"./index-d3ea75b5.js";import"./index-d37d4223.js";import"./extends-98964cd2.js";import"./index-356e4a49.js";export{l as WithToolTipState,s as WithTooltip,f as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-4HIR6TLV-17a888a7.js.map
