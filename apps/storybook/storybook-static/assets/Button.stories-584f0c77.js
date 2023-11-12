5245a538a7b4eb9991a6f1ad40c2071afa3aaa2c
Anthonius Munthi <me@itstoni.com>
2023-11-13 01:23:16 +0800

    chore: prepare github release action

---
+++ apps/storybook/storybook-static/assets/Button.stories-584f0c77.js
@@ -0,0 +1,14 @@
+import{B as u}from"./ValidatedInputText-62d093cd.js";import"./index-edd43725.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./tiny-invariant-dd7d57d2.js";import"./iframe-a370f03a.js";import"../sb-preview/runtime.js";import"./extends-98964cd2.js";import"./index-d3ea75b5.js";const _={title:"Base/Button",component:u},e={args:{action:"save"}},r={args:{action:"delete"}},t={args:{action:"reset"}};var a,o,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    action: "save"
+  }
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var n,c,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    action: "delete"
+  }
+}`,...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var p,i,d;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    action: "reset"
+  }
+}`,...(d=(i=t.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};const E=["Save","Delete","Reset"];export{r as Delete,t as Reset,e as Save,E as __namedExportsOrder,_ as default};
+//# sourceMappingURL=Button.stories-584f0c77.js.map
