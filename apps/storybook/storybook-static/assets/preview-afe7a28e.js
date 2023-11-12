57d86f7b390476825789657df30a5fa4bf11f290
Anthonius Munthi <me@itstoni.com>
2023-11-13 00:28:50 +0700

    chore: prepare github release action (#3)

---
+++ apps/storybook/storybook-static/assets/preview-afe7a28e.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-a370f03a.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-5ee48ba5.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-5ee48ba5.js","./iframe-a370f03a.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./_getPrototype-e1e43787.js","./index-d3ea75b5.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-afe7a28e.js.map
