f357351e2da651f6f9c5e170d88e03ced375a814
Anthonius Munthi <me@itstoni.com>
2025-08-07 15:07:21 +0800

    feat(web): created omed website with astro starlight template :rocket:

---
+++ apps/web/astro.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Omed",
+      tableOfContents: false,
+      pagefind: false,
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/kilip/omed",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+    react(),
+  ],
+});
